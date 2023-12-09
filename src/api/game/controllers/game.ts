c1266b7af6a6030c20f13a74c43be6b90b38ad2c
tiblazy <hauntedapply@gmail.com>
2023-12-09 16:25:34 -0300

    example controller and service strapi

--- src/api/game/controllers/game.ts
+++ src/api/game/controllers/game.ts
@@ -2,6 +2,17 @@
  * game controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::game.game');
+export default factories.createCoreController(
+  "api::game.game",
+  ({ strapi }) => ({
+    async populate(ctx) {
+      console.log("Rodando");
+
+      // await strapi.service("api::game.game").populate(ctx.query);
+
+      ctx.send(await strapi.service("api::game.game").populate(ctx.query));
+    },
+  })
+);
--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -2,6 +2,17 @@
  * game service
  */
 
-import { factories } from '@strapi/strapi';
+import axios from "axios";
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreService('api::game.game');
+export default factories.createCoreService("api::game.game", () => ({
+  async populate(params) {
+    const gogApiUrl = `https://catalog.gog.com/v1/catalog?limit=48&genres=in%3Aaction&order=desc%3Atrending&productType=in%3Agame%2Cpack%2Cdlc%2Cextras&page=1&countryCode=BR&locale=en-US&currencyCode=BRL`;
+
+    const {
+      data: { products },
+    } = await axios.get(gogApiUrl);
+
+    console.log(products)
+  },
+}));
