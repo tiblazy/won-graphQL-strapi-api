dd81064c4725e28d7b4b92ffd9165498a7308517
tiblazy <hauntedapply@gmail.com>
2023-12-09 17:44:53 -0300

    example fetch jsdom element

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -2,8 +2,35 @@
  * game service
  */
 
-import axios from "axios";
 import { factories } from "@strapi/strapi";
+import axios from "axios";
+import { JSDOM } from "jsdom";
+
+async function getGameInfo(slug) {
+  // const gogSlug = slug.replaceAll("-", "_").toLowerCase();
+
+  const body = await axios.get(`https://www.gog.com/en/game/${slug}`);
+  const dom = new JSDOM(body.data);
+
+  const raw_description = dom.window.document.querySelector(".description");
+  const description = raw_description.innerHTML;
+  const short_description = raw_description.textContent.slice(0, 160);
+
+  const ratingElement = dom.window.document.querySelector(
+    ".age-restrictions__icon use"
+  );
+
+  return {
+    description,
+    short_description,
+    rating: ratingElement
+      ? ratingElement
+          .getAttribute("xlink:href")
+          .replace(/_/g, "")
+          .replace("#", "")
+      : "BR0",
+  };
+}
 
 export default factories.createCoreService("api::game.game", () => ({
   async populate(params) {
@@ -13,6 +40,6 @@ export default factories.createCoreService("api::game.game", () => ({
       data: { products },
     } = await axios.get(gogApiUrl);
 
-    console.log(products)
+    console.log(await getGameInfo(products[0].slug));
   },
 }));
